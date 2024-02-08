845815a8f627cb50fac2102dea3cc5bd1e4c5c04
Vitor Arimatea <vitorodev@outlook.com>
2024-02-07 23:19:53 -0300

    new: Criando o modal

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,8 +1,75 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { X } from 'lucide-react';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { toast } from 'sonner';
+
 export function NewNoteCard() {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+  const [content, setContent] = useState('')
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false)
+  }
+
+  function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
+    setContent(event.target.value)
+
+    if (event.target.value === '') {
+      setShouldShowOnboarding(true)
+    }
+  }
+
+  function handleSaveNote(event: FormEvent) {
+    event.preventDefault()
+
+    toast.success('Nota criada com sucesso!')
+  }
+
   return (
-    <button className='text-left flex flex-col align-top bg-slate-700 rounded-md p-5 space-y-3 text-sm'> 
-      <span className='text-slate-200 font-medium'>Adicionar nota</span>
-      <p className='text-slate-400 leading-6'>Grave uma nota em áudio que será convertida para texto automaticamente.</p>
-    </button>
+    <Dialog.Root>
+      <Dialog.Trigger className='text-left flex flex-col align-top bg-slate-700 rounded-md p-5 space-y-3 text-sm hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400 outline-none'> 
+        <span className='text-slate-200 font-medium'>Adicionar nota</span>
+        <p className='text-slate-400 leading-6'>Grave uma nota em áudio que será convertida para texto automaticamente.</p>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className='inset-0 fixed bg-black/50' />
+        <Dialog.Content className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 outline-none rounded-md flex flex-col overflow-hidden'>
+          
+          <Dialog.Close className='absolute right-0 bg-slate-800 text-slate-500 p-1.5 hover:text-slate-100'>
+            <X className='size-5' />
+          </Dialog.Close>
+
+          <form onSubmit={handleSaveNote} className='flex flex-col flex-1'>
+
+            <div className='flex flex-1 flex-col gap-3 p-5'>
+              <span className='text-slate-300 font-medium text-sm'>
+                Adicionar nota
+              </span>
+              
+              {shouldShowOnboarding ? (
+                <p className='text-slate-400 leading-6 text-sm'>
+                Comece <button className='text-lime-400 font-medium hover:underline'>gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className='text-lime-400 font-medium hover:underline'>utilize apenas texto</button>.
+              </p>
+              ) : (
+                <textarea 
+                  autoFocus
+                  className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
+                  onChange={handleContentChanged} 
+                />  
+              )}
+            </div>
+
+            <button 
+              type="submit"
+              className='w-full bg-lime-400 text-[#1A2E05] py-4 text-sm font-medium hover:bg-lime-500'  
+            >
+              Salvar nota
+            </button>
+          </form>
+
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,10 +1,53 @@
-export function NoteCard() {
+import * as Dialog from '@radix-ui/react-dialog';
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { X } from 'lucide-react';
+
+interface NoteCardProps {
+  note: {
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className='flex flex-col align-top text-left align-items-flex-start  bg-slate-800 rounded-md p-5 space-y-3 text-sm overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400 outline-none'> 
-      <span className='text-slate-300 font-medium'>Há 3 dias</span>
-      <p className='text-slate-400 leading-6'>No app do NLW vamos criar um layout incrível, assim podemos entregar a melhor experiência para a comunidade. Na aplicação React vamos criar um projeto que permite o usuário salvar notas em texto ou áudio.</p>
+    <Dialog.Root>
+      <Dialog.Trigger 
+        className='flex flex-col text-left align-items-flex-start  bg-slate-800 rounded-md p-5 gap-3 text-sm overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400 outline-none'
+      > 
+        <span className='text-slate-300 font-medium'>{formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}</span>
+        <p className='text-slate-400 leading-6'>{note.content}</p>
+            
+        <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'></div>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className='inset-0 fixed bg-black/50' />
+        <Dialog.Content className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 outline-none rounded-md flex flex-col overflow-hidden'>
           
-      <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'></div>
-    </button>
+          <Dialog.Close className='absolute right-0 bg-slate-800 text-slate-500 p-1.5 hover:text-slate-100'>
+            <X className='size-5' />
+          </Dialog.Close>
+
+          <div className='flex flex-1 flex-col gap-3 p-5'>
+            <span className='text-slate-300 font-medium'>
+              {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+            </span>
+            <p className='text-slate-400 leading-6'>
+              {note.content}
+            </p>
+          </div>
+
+          <button 
+            type="button"
+            className='w-full bg-slate-800 text-slate-300 py-4 text-sm font-medium group'  
+          >
+            Deseja <span className='text-red-400 group-hover:underline'>apagar essa nota</span>?
+          </button>
+
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   ) 
 }
